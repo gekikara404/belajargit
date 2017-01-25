b1a6b51cdff6cc74eb43964b316b37341ba8c5e3
gekikara404 <gekikara404@gmail.com>
2017-01-25 00:07:24 -0500

    update contact.php

---
+++ contact.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Ini contact</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus sit commodi debitis, cum iste distinctio repellendus neque dolor, nihil quis maxime modi laborum consequuntur quos officia ratione quisquam eveniet adipisci!</p>
+</body>
+</html>
