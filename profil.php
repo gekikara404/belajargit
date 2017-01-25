b7b0e4ffbe8e80ef8e0db00dfa73d709948d6119
gekikara404 <gekikara404@gmail.com>
2017-01-25 00:00:11 -0500

    update index dan file profile

---
+++ profil.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>profil</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi ipsam beatae aspernatur ex hic impedit, nemo, provident maiores dolor quae velit officiis doloribus, reiciendis enim veritatis commodi! Officiis, tenetur, hic!</p>
+</body>
+</html>
